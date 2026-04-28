da05a2406a71f9141cf683a015d28e2d03f5053a
junexmsc-rgb <junexmsc@gmail.com>
2026-04-28 13:53:12 +0100

    Create tailwind-config.js

---
+++ tailwind-config.js
@@ -0,0 +1,14 @@
+// tailwind-config.js
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ['Cinzel', 'serif'], // High-end architectural serif
+                body: ['Montserrat', 'sans-serif'], // Geometric, structural body text
+            },
+            colors: {
+                terracotta: '#C17767', // A warm, sophisticated accent color mimicking clay/brick
+            }
+        }
+    }
+}
